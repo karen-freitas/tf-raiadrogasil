0fbb37d73d492cf4ab833d19b16f6833a041dddd
Karen Freitas <karenfcorrea@gmail.com>
2021-11-08 19:18:09 -0300

    configurações e teste firebase

---
+++ src/config.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+
+export const authConfig = firebase.initializeApp({
+  apiKey: "AIzaSyDb-bERSVa7ivDiHB-ENIH8MsqdWn1CcOY",
+  authDomain: "tf-raiadrogasil.firebaseapp.com",
+  projectId: "tf-raiadrogasil",
+  storageBucket: "tf-raiadrogasil.appspot.com",
+  messagingSenderId: "56887773199",
+  appId: "1:56887773199:web:948b039f29047189397b7a"
+});
+
+
---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import { authConfig } from '../config';
+
+const db = authConfig.firestore()
+db.collection('teste')
+console.log(db.collection('teste'))
+
+
+export const registerEmployee = (employeeName, employeeLastName, employeeEmail, employeePhone  ) => db
+    .collection('employees').add({
+      name:employeeName,
+      lastName:employeeLastName,
+      email:employeeEmail,
+      phone:employeePhone  
+    })
+  
+
+   
