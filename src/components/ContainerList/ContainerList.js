35e174fa19c433beffc6dfea5c63fa5a7ac54a08
belobruna <bruna.belo@gmail.com>
2021-11-10 15:41:57 -0300

    merge alterações lista

--- src/components/ContainerList/ContainerList.js
+++ src/components/ContainerList/ContainerList.js
@@ -1,6 +1,6 @@
 import { Box } from '@material-ui/system';
 import React from 'react';
-import '../../styles/ContainerList.css';
+import '../../styles/containerList.css';
 
 const ContainerList = ({ children }) => {
   return <Box className="container-list">{children}</Box>;
